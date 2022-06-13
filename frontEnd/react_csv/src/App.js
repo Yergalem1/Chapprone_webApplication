a2296c0f6b6beb5b61ee0294a9e7495d0540fd75
Yergalem1 <yergalemt@gmail.com>
2022-06-13 06:01:31 -0700

    data visualization update

--- frontEnd/react_csv/src/App.js
+++ frontEnd/react_csv/src/App.js
@@ -4,6 +4,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import CSV from './components/csv.component.js'
 import Recharts from './components/rechart.component.js'
+
+
 function App() {
   return (
     <Router>
@@ -19,25 +21,28 @@ function App() {
                   </Link>
                 </li>
               
-                <li className="nav-item">
+                <li >
                   <Link className="nav-link" to={'/rechart'}>
                  Chart 
                   </Link>
                 </li>
-               
+           
               </ul>
             </div>
           </div>
         </nav>
         <div className="auth-wrapper">
+     
           <div className="auth-inner">
             <Routes>
               <Route exact path="/" element={<CSV />} />
               <Route path="/csv" element={<CSV />} />
              
-              <Route path="/rechart" element={<Recharts />} />
+          
             </Routes>
           </div>
+          <div><Routes>  <Route path="/rechart" element={<Recharts />} /></Routes></div>
+          
         </div>
       </div>
     </Router>
--- frontEnd/react_csv/src/components/rechart.component.js
+++ frontEnd/react_csv/src/components/rechart.component.js
@@ -1,65 +1,75 @@
-import React, { useState, useEffect} from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
 
- const data = [
-    {
-      name: "March",
-      Iphone: 1000,
-    },
-    {
-      name: "May",
-      Iphone: 4000,
-    },
-    {
-      name: "July",
-      Iphone: 800,
-    },
-    {
-      name: "October",
-      Iphone: 1500,
-    },
-  ]; 
-  
-const Rechart = () =>  {
-    const [charts, setCharts] = useState([])
+  BarChart, Bar, Cell
+} from "recharts";
 
-    const fetchData = async () => {
-       const BASE_URL = 'http://localhost:5001/chapperone-6a910/us-central1/api'
-      const res = await fetch(`${BASE_URL}/all_users`)
-      const data = await res.json()
-      console.log(data.data)
-      setCharts(data.data)
-    }
-  
-    useEffect(() => {
-      fetchData()
-    }, [])
-     
-   return (
-     <>
-      <ResponsiveContainer width="100%" aspect={3}>
-     <LineChart
-        width={3000}
-        height={3000}
-        data={data}
-        margin={{
-          top: 15,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid  horizontal="true" vertical="" stroke="#243240"/>
-        <XAxis dataKey="name" tick={{fill:"#fff"}}/>
-        <YAxis tick={{fill:"#fff"}} />
-        <Tooltip contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }} itemStyle={{ color: "#fff" }} cursor={false}/>
-        <Line type="monotone" dataKey="start_date" stroke="#8884d8" strokeWidth="5" dot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 2,r:5}} activeDot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 5,r:10}} />
-        
-      </LineChart>
-    </ResponsiveContainer>
-   
-  </>
-  );
-}
+
+
+
+const Rechart = () => {
+
+
+  const [charts, setCharts] = useState([]);
+
+  const fetchData = async () => {
+    const BASE_URL = "http://localhost:5001/chapperone-6a910/us-central1/api";
+    const res = await fetch(`${BASE_URL}/all_trips`);
+    const data = await res.json();
+    console.log("data from api", data.results);
+    setCharts(data.results);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //  render() {
+  return (
+    <>
+   <h2 className="header">Trip Information Count</h2>
+   <ResponsiveContainer width="100%" aspect={3}>
+
+    <LineChart
+    width={1000}
+    height={200}
+    data={charts}
+    margin={{
+      top: 5,
+      right: 30,
+      left: 20,
+      bottom: 5
+    }}
+  >
+  <CartesianGrid horizontal="true"  vertical="" stroke="#eee" strokeDasharray="5 5" />
+    <XAxis dataKey="Start_date"tick={{fill:"#fff"}}/>
+    <YAxis tick={{fill:"#fff"}} />
+     <Tooltip contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }} itemStyle={{ color: "#fff" }} cursor={false}/>
+    <Legend />
+    <Line
+      type="monotone"
+      dataKey="Number_Of_Student"
+      stroke="#db0b27"
+      activeDot={{ r: 8 }}
+    />
+    <Line type="monotone" dataKey="Number_Of_Teacher" stroke="#3f6641" />
+    <Line type="monotone" dataKey="broadcastsCount" stroke="#3f6641" />
+    <Line type="monotone" dataKey="messageCount" stroke="#3f6641" />
+  </LineChart>
+ </ResponsiveContainer> 
+
+    
+  </>);
+};
 
 export default Rechart;
+
